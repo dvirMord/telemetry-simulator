1c8d723a0e2bf159cb174c2e1d797592d176a51d
Dvir Mordoh <dvirmordoh2011@gmail.com>
2026-08-27 15:01:34 +0300

    [fix] uploadFile: now it returns decoedFile Id from the DB

--- app/Controllers/TelemetryFilesController.py
+++ app/Controllers/TelemetryFilesController.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, UploadFile, File, Depends, status
-from app.Interfaces.Itelemetry_files_service import ITelemetryFilesService
+from fastapi import APIRouter, Depends, File, UploadFile, status
+
+from app.Core.config import settings
 from app.dependencies import get_telemetry_files_service
 from app.DTOs.DeleteFileDTO import DeleteFileDTO
-from app.Core.config import settings
+from app.Interfaces.Itelemetry_files_service import ITelemetryFilesService
+
 current_version = settings.CURRENT_VERSION
 router = APIRouter(
     prefix=f"/api/{current_version}/ts",
@@ -11,12 +13,12 @@ router = APIRouter(
 
 #--------------Recive and save file--------------------------
 @router.post("/files", status_code=status.HTTP_200_OK)
-async def recive_klv_file(file: UploadFile = File(...), files_service: ITelemetryFilesService = Depends(get_telemetry_files_service)):
+async def recive_klv_file(file: UploadFile = File(...), files_service: ITelemetryFilesService = Depends(get_telemetry_files_service)):  # noqa: B008
     return await files_service.Recive_file(file)
 #----------------end-----------------------------------------
 
 #-----------------delete file from the service-------------------
 @router.delete("/files", status_code=status.HTTP_200_OK)
-async def delete_klv_file(dto: DeleteFileDTO, files_service: ITelemetryFilesService = Depends(get_telemetry_files_service)):
+async def delete_klv_file(dto: DeleteFileDTO, files_service: ITelemetryFilesService = Depends(get_telemetry_files_service)):  # noqa: B008
     return await files_service.Delete_file(dto.file_name)
 #------------------end---------------------------------------
